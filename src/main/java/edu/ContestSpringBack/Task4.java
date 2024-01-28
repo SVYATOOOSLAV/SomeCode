571cc0df91253325ac2383b9243df1cd96b0f9ef
Svyatoslav Milkin <svyatoslavmilkin@yandex.ru>
2024-01-28 20:07:14 +0300

    Rewrite Task4 for contestBack

--- src/main/java/edu/ContestSpringBack/Task4.java
+++ src/main/java/edu/ContestSpringBack/Task4.java
@@ -16,13 +16,13 @@ public class Task4 {
         Graph graph = new Graph();
         long start = System.currentTimeMillis();
         //create friends
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             graph.add(new Node(dataInterestingness[i], dataVoracity[i]));
         }
 
         //create connection
-        for(int i = 0; i < m; i++){
-            int[] friends =Arrays.stream(scanner.nextLine().split(" "))
+        for (int i = 0; i < m; i++) {
+            int[] friends = Arrays.stream(scanner.nextLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
             graph.createNeighbors(friends[0] - 1, friends[1] - 1);
         }
@@ -30,54 +30,55 @@ public class Task4 {
         graph.startDepthFirstTraversal();
 
         List<ConnectionInfo> connectionInfo = graph.getAllInfo();
-        connectionInfo.sort(new Comparator<ConnectionInfo>() {
+
+        Comparator<ConnectionInfo> compVoracity = new Comparator<ConnectionInfo>() {
             @Override
             public int compare(ConnectionInfo o1, ConnectionInfo o2) {
                 return Integer.compare(o1.getSumVoracity(), o2.getSumVoracity());
             }
-        });
+        };
 
-        int result = getResult(connectionInfo, g);
+        Comparator<ConnectionInfo> compInter = new Comparator<ConnectionInfo>() {
+            @Override
+            public int compare(ConnectionInfo o1, ConnectionInfo o2) {
+                return Integer.compare(o2.getSumInterestingness(), o1.getSumInterestingness());
+            }
+        };
+
+        connectionInfo.sort(compInter);
+        int result1 = getResult(connectionInfo, g);
+
+        connectionInfo.sort(compVoracity);
+        int result2 = getResult(connectionInfo, g);
+
         long end = System.currentTimeMillis();
-        System.out.println(end-start);
-        System.out.println(result);
+        System.out.println(end - start);
+        System.out.println(Math.max(result1, result2));
     }
 
     private static int getResult(List<ConnectionInfo> connectionInfo, int g) {
         int result = 0;
         for(int i = 0; i < connectionInfo.size(); i++){
-            int firstValInteres = connectionInfo.get(i).getSumInterestingness();
-            int firstValVoracity = connectionInfo.get(i).getSumVoracity();
-            if(firstValInteres > result && firstValVoracity <= g){
-                result = firstValInteres;
+            if(connectionInfo.get(i).getSumVoracity() > g){
+                continue;
             }
-            for (int j = i + 1; j < connectionInfo.size(); j++){
-                int secondValInteres = connectionInfo.get(j).getSumInterestingness();
-                int secondValVoracity = connectionInfo.get(j).getSumVoracity();
-                if(firstValInteres + secondValInteres > result && firstValVoracity + secondValVoracity<= g){
-                    result = firstValInteres + secondValInteres;
+            int sumInteres = connectionInfo.get(i).getSumInterestingness(),
+                    sumVorac = connectionInfo.get(i).getSumVoracity();
+            for(int j = i + 1; j < connectionInfo.size(); j++){
+                if(sumVorac + connectionInfo.get(j).getSumVoracity() <= g){
+                    sumVorac +=  connectionInfo.get(j).getSumVoracity();
+                    sumInteres += connectionInfo.get(j).getSumInterestingness();
                 }
             }
-        }
-
-        for(int i = 0; i < connectionInfo.size(); i++){
-            int firstValInteres = connectionInfo.get(i).getSumInterestingness();
-            int firstValVoracity = connectionInfo.get(i).getSumVoracity();
-            for (int j = i + 1; j < connectionInfo.size(); j++){
-                int secondValInteres = connectionInfo.get(j).getSumInterestingness();
-                int secondValVoracity = connectionInfo.get(j).getSumVoracity();
-                firstValInteres += secondValInteres;
-                firstValVoracity += secondValVoracity;
-                if(firstValInteres > result && firstValVoracity <= g){
-                    result = firstValInteres;
-                }
+            if(result < sumInteres){
+                result = sumInteres;
             }
         }
         return result;
     }
 }
 
-class ConnectionInfo{
+class ConnectionInfo {
     private int sumInterestingness;
     private int sumVoracity;
 
@@ -89,11 +90,11 @@ class ConnectionInfo{
         return sumVoracity;
     }
 
-    public void addInterestingness(int value){
+    public void addInterestingness(int value) {
         sumInterestingness += value;
     }
 
-    public void addVoracity(int value){
+    public void addVoracity(int value) {
         sumVoracity += value;
     }
 }
@@ -113,6 +114,7 @@ class Graph {
 
     private List<Node> graph = new ArrayList<>();
     private List<ConnectionInfo> allInfo = new ArrayList<>();
+
     public void add(Node node) {
         graph.add(node);
     }
@@ -126,11 +128,10 @@ class Graph {
         graph.get(j).neighbors.add(graph.get(i));
     }
 
-    public void startDepthFirstTraversal(){
+    public void startDepthFirstTraversal() {
         List<Node> tabu = new ArrayList<>();
-        for(Node node : graph)
-        {
-            if(!tabu.contains(node)){
+        for (Node node : graph) {
+            if (!tabu.contains(node)) {
                 ConnectionInfo info = new ConnectionInfo();
                 tabu.add(node);
                 info.addInterestingness(node.interestingness);
@@ -142,9 +143,9 @@ class Graph {
         }
     }
 
-    private void depthFirstTraversal(Node node, List<Node> tabu, ConnectionInfo info){
-        for(Node friend : node.neighbors){
-            if(!tabu.contains(friend)){
+    private void depthFirstTraversal(Node node, List<Node> tabu, ConnectionInfo info) {
+        for (Node friend : node.neighbors) {
+            if (!tabu.contains(friend)) {
                 tabu.add(friend);
                 info.addInterestingness(friend.interestingness);
                 info.addVoracity(friend.voracity);
